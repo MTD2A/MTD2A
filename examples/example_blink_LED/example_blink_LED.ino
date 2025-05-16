eee2d4c0d4baa7d10f14b384b8e7ae97b040088b
Jørgen Bo Madsen <99587453+jebmdk@users.noreply.github.com>
2025-05-16 09:44:45 +0200

    Add files via upload

--- examples/example_blink_LED/example_blink_LED.ino
+++ examples/example_blink_LED/example_blink_LED.ino
@@ -1,20 +1,28 @@
 // example_blink_LED.ino ----------------------------------------------------------------------------- 
 
-// Blinking LED with 1 second interval
+// Two blinking LEDs. One with symmetric interval and another with asymetric interval.
 
 #include <MTD2A.h>
 
-MTD2A_binary_output red_LED ("Red LED", 500, 500);  // 0.5 sec light, 0.5 sec no light
+MTD2A_binary_output red_LED   ("Red LED",   400, 400);  // 0.4 sec light, 0.4 sec no light
+MTD2A_binary_output green_LED ("Green LED", 300, 700);  // 0.3 sec light, 0.7 sec no light
 
 void setup() {
   Serial.begin(250000);
-  red_LED.set_debugPrint (enable); // Enable debug print for all instantiated classes
-  red_LED.initialize (9);  // Output pin 9 and loop process
-  Serial.println("LED blink");
+
+  red_LED.initialize (9);     // Output pin 9
+  green_LED.initialize (10);  // Output pin 10
+
+  Serial.println("Two LED blink");
 }
 
 void loop() {
-  if (red_LED.get_processState() == pending) red_LED.activate();
+  if (red_LED.get_processState()   == pending)   red_LED.activate();
+  if (green_LED.get_processState() == pending) green_LED.activate();
+
   red_LED.loop_fast();
+  green_LED.loop_fast();
+  
   delay (10);
-} // Blinking LED with 1 second interval
+} 
+
