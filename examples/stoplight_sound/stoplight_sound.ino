b9f34d96a7b0655617b8094498a31ad6e34cba51
Jørgen Bo Madsen <99587453+jebmdk@users.noreply.github.com>
2025-06-14 21:49:49 +0200

    Add files via upload

--- examples/stoplight_sound/stoplight_sound.ino
+++ examples/stoplight_sound/stoplight_sound.ino
@@ -1,32 +1,37 @@
 
 // Stop light and sound message: The train brakes and temporarily stops at a red light.
+// Speaker announcement is broadcast to the passengers. If the train is passing by, no announcement is broadcast.
 // Jørgen Bo Madsen / may 2025 / https://github.com/jebmdk
 
 #include <MTD2A.h>
 using namespace MTD2A_const;
 
-MTD2A_binary_input  FC_51_stop  ("Stop train",  10000);   
-// continuous active time. No incorrect reactivation due to gaps between the wagons.
-MTD2A_binary_output train_sound ("Sound", 100, 5000);  // Wait 5 seconds and activate 0,1 second
-// Trigger sound board - non-blocking - 5 seconds after train detection for duraten of 0,1 seconds
+MTD2A_binary_input  FC_51_stop  ("Stop train",  2000);   
+// continuous active time. No incorrect reactivation due to gaps between the train wagons.
+MTD2A_binary_output train_sound ("Sound", 100, 0, 15000); 
+// Trigger "DFplayer mini" "Buton activation" method for duraten of 0,1 seconds. Not less - not more.
+// Wait 15 seconds i total before next announcement. Loop until train has moved on.
+unsigned long timerStart = 0;     // Determine if the train is stationary or is passing by the stop light.
+unsigned long DelayTime  = 8000;  // Check if the train is stationary 8 seconds or moere after detection.
 
 void setup() {
   Serial.begin(9600);
-  MTD2A_globalDebugPrint(); // Enable debug print for all instantiated classes
-
-  byte FC_51_STOP_PIN  = 2;   // Arduino board input pin 2
-  byte TRAIN_SOUND_PIN = A0;  // Arduino board Output pin Analog and digtal pin A0 
+  byte FC_51_STOP_PIN  = A0; // Arduino board input analog and digtal pin A0
+  byte TRAIN_SOUND_PIN = 2;  // Arduino board output pin 2
   FC_51_stop.initialize  (FC_51_STOP_PIN);
-  train_sound.initialize (TRAIN_SOUND_PIN, INVERTED);
+  train_sound.initialize (TRAIN_SOUND_PIN, INVERTED); // trigger = HIGH to LOW
   // Danish and English speech from https://ttsmaker.com/
 }
 
 void loop() {
-  if (FC_51_stop.get_processState() == ACTIVE ) { // Continous activation (no drop outs)
-    if (FC_51_stop.get_phaseChange() == true &&  FC_51_stop.get_phaseNumber() == FIRST_TIME_PHASE) {
-      train_sound.activate();
+  if (FC_51_stop.get_processState() == ACTIVE ) { 
+    if (FC_51_stop.get_phaseChange() == true && FC_51_stop.get_phaseNumber() == FIRST_TIME_PHASE) {
+      timerStart = millis();
     }
-  }
+    if (millis() - timerStart > DelayTime) {
+      train_sound.activate();  // Speaker announcement is broadcast to the passengers. Looping.
+    }
+  }  // Active
 
   MTD2A_loop_execute();  // Update the state (event) system
 } // Stop light and sound message: The train brakes and temporarily stops at a red light.
